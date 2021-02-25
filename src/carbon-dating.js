b4cd2fc2de5a3a9dd548aa16b8e8364f5293e74b
Adolf <dgtronv@gmail.com>
2021-02-26 02:25:42 +0300

    fix: fix wrong wtype error in carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,11 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  if (typeof sampleActivity !== 'number') {
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string'
+    || Number(sampleActivity) <= 0
+    || isNaN(sampleActivity)
+    || Number(sampleActivity) > MODERN_ACTIVITY) {
     return false;
   }
-  const carbonYear = 0.693 / HALF_LIFE_PERIOD;
-  const Year = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / carbonYear);
-  return Year;
+
+  let approximateAge = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+
+  return approximateAge;
+
 };
+
