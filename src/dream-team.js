2da81c91a3f28d259ebf05d3a609c4e6d655d31e
zoldief <57657757+zoldief@users.noreply.github.com>
2021-03-14 00:22:12 +0300

    fix: update dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,20 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if (typeof members !== 'object' || !members || members == null) {
-    return false;
-  }
-  let team = '';
-  let arr = [];
-  for (let i = 0; i < members.length; i++) {
-    if (typeof members[i] !== 'number' && members[i] && typeof members[i] === 'string') {
-      arr.push(members[i].trim().toUpperCase().charAt(0));
+    let arr = [];
+    if (typeof members !== 'object' || !members || members == null) {
+        return false;
+    } else {
+        for (let i = 0; i < members.length; i++) {
+            if (typeof members[i] !== 'number' && members[i] && typeof members[i] === 'string') {
+                arr.push(members[i].trim().toUpperCase().charAt(0));
+            }
+        }
+        arr.sort();
     }
-  }
-  arr.sort();
-  for (let i = 0; i < arr.length; i++) {
-    team = team + arr[i];
-  }
-  return team;
+    return arr.join('');
 };
 
