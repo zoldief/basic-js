011d408cdf3556b1c72fb542c5d4f091a652b8b7
zoldief <57657757+zoldief@users.noreply.github.com>
2021-03-14 00:26:06 +0300

    fix: update recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr, count = 1, res = []) {
-    res.push(count);
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] instanceof Array) {
-        this.calculateDepth(arr[i], count + 1, res);
+  calculateDepth(array, counter = 1, result = []) {
+    result.push(counter);
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] instanceof Array) {
+        this.calculateDepth(array[i], counter + 1, result);
       }
     }
-
-    return res.sort((a, b) => b - a).pop();
+    result = result.sort((a, b) => b - a).pop();
+    return result
   }
 };
